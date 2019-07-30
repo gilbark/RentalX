289b6427320201327f2427f97f98c31ca76bc152
Gil Barkan <barkan.gil@gmail.com>
2019-07-30 14:22:01 +0200

    Add project files.

---
+++ RentalX/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RentalX.MvcApplication" Language="C#" %>
---
+++ RentalX/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RentalX.Startup))]
+namespace RentalX
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
